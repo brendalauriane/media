abaeec7697c7b8060fc53000d320e93d41f3055e
brendalauriane <atblauriane@gmail.com>
2025-03-04 14:39:43 +0100

    cofigurons le middlewaare

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function Aboutpage() {
+  return (
+    <div>
+     
+    </div>
+  );
+}
+
+export default Aboutpage;
